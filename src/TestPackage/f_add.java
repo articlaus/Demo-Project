4c116471021d5b264dc386813cf17b622595dac0
Ganbat Bayar <arti_tj@yahoo.com>
2011-12-20 14:09:46 +0530

    Testing Commit from Idea

---
+++ src/TestPackage/f_add.java
@@ -0,0 +1,16 @@
+package TestPackage;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Articlaus
+ * Date: 12/20/11
+ * Time: 2:00 PM
+ */
+public class f_add {
+    public static void main(String[] args) {
+        int c = 5;
+        int a = 4;
+        c = c + a;
+        System.out.println(c);
+    }
+}
